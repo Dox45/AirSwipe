ef651b6818c5cab9c210ad350b7a386f06d79f30
purpleWavelet <fender54eurois@gmail.com>
2025-03-18 22:30:58 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during production builds
+  },
   images: {
     remotePatterns: [
       { hostname: "exciting-caterpillar-231.convex.cloud", protocol: "https" },
